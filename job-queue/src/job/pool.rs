5254d1c5de3601d078dab78d780fc185081868ca
synerr <imposter@amogus.sex>
2025-06-12 16:00:36 +0300

    Finally! I can now use rayon::TreadPool

--- job-queue/src/job/pool.rs
+++ job-queue/src/job/pool.rs
@@ -21,13 +21,13 @@ struct Worker<H: AsyncJobHandler> {
 // right now it's an unordered subscription based model: you send some data -> you get some unordered output
 pub struct Pool<H: AsyncJobHandler> {
     workers_channels: Vec<mpsc::Sender<H::Input>>,
-    pub result_chan: mpsc::Receiver<H::Output>,
-    cancell: CancellationToken,
+    pub worker_result_receiver: tokio::sync::Mutex<mpsc::Receiver<H::Output>>,
 }
 
 impl<H: AsyncJobHandler> Worker<H>
 where
     H::Input: std::fmt::Debug,
+    H::Output: std::fmt::Debug,
 {
     pub async fn run(&mut self) {
         loop {
@@ -35,9 +35,8 @@ where
                 _ = self.cancell.cancelled() => {
                     break;
                 }
-                input = self.task_chan.recv() => {
+                input = self.task_chan.recv()=> {
                     if let Some(input) = input {
-                        println!("Received an input in worker: {:#?}", input);
                         let output = self.handler.handle(input).await;
                         self.result_chan.send(output).await.unwrap();
                     }
@@ -45,44 +44,42 @@ where
             }
         }
     }
-    pub fn stop(&self) {
-        self.cancell.cancel()
-    }
 }
 impl<H: AsyncJobHandler> Pool<H>
 where
     H::Input: std::fmt::Debug,
+    H::Output: std::fmt::Debug,
     H: Sync + Send + 'static,
 {
     pub fn new(handler: H, num_workers: usize, cancell: CancellationToken) -> Self {
-        let (result_sender, result_receiver) = mpsc::channel(num_workers * 2);
+        let (worker_result_sender, worker_result_receiver) =
+            mpsc::channel::<H::Output>(num_workers * 2);
         let worker_arc = Arc::new(handler);
 
         let mut workers_channels: Vec<mpsc::Sender<H::Input>> = vec![];
 
         for _ in 0..num_workers {
-            let (task_sender, task_receiver) = mpsc::channel(num_workers * 2);
+            let (task_sender, task_receiver) = mpsc::channel::<H::Input>(num_workers * 2);
             workers_channels.push(task_sender);
 
             let mut new_worker = Worker {
-                result_chan: result_sender.clone(),
+                result_chan: worker_result_sender.clone(),
                 handler: worker_arc.clone(),
                 cancell: cancell.child_token(),
                 task_chan: task_receiver,
             };
             tokio::spawn(async move {
-                new_worker.run();
+                new_worker.run().await;
             });
         }
         Self {
-            result_chan: result_receiver,
+            worker_result_receiver: tokio::sync::Mutex::new(worker_result_receiver),
             workers_channels: workers_channels,
-            cancell: cancell,
         }
     }
 
-    pub fn queue(&self, input: H::Input) {
+    pub async fn queue(&self, input: H::Input) {
         let index = fastrand::usize(..self.workers_channels.len());
-        let _ = self.workers_channels[index].send(input);
+        let _ = self.workers_channels[index].send(input).await;
     }
 }
--- job-queue/src/main.rs
+++ job-queue/src/main.rs
@@ -1,5 +1,45 @@
 mod job;
 
-fn main() {
-    println!("Hello, world!");
+use std::{sync::Arc, time::Duration};
+use tokio::time::timeout;
+use tokio_util::sync::CancellationToken;
+
+use crate::job::pool::{AsyncJobHandler, Pool};
+
+struct SquareCalc;
+
+#[async_trait::async_trait]
+impl AsyncJobHandler for SquareCalc {
+    type Input = i32;
+    type Output = i32;
+
+    async fn handle(&self, input: Self::Input) -> Self::Output {
+        input * input
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let token = CancellationToken::new();
+    let pool = Arc::new(Pool::new(SquareCalc {}, 8, token.clone()));
+    let pool_clone = pool.clone();
+    tokio::spawn(async move {
+        for _ in 0..10_000 {
+            let _ = pool.queue(fastrand::i32(-46339..46340 - 1)).await;
+        }
+    });
+    let f = tokio::spawn(async move {
+        let mut receiver = pool_clone.worker_result_receiver.lock().await;
+        let mut counter = 0;
+        while counter < 10_000 {
+            if let Some(_) = receiver.recv().await {
+                counter += 1;
+            }
+        }
+        println!("DONE!!!!!!");
+        token.cancel();
+    });
+    if let Err(_) = timeout(Duration::from_secs(5), f).await {
+        println!("timeout...;")
+    }
 }
