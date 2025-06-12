76e9d85705f2b758d0c9a3d95cadf864a69cf715
synerr <imposter@amogus.sex>
2025-06-12 14:49:43 +0300

    Why didn't I think of this earlier...

--- job-queue/src/job/pool.rs
+++ job-queue/src/job/pool.rs
@@ -1,44 +1,35 @@
 use std::sync::Arc;
 
-use tokio::sync::{
-    mpsc::{self, Receiver, Sender},
-    watch,
-};
+use tokio::sync::mpsc::{self};
 use tokio_util::sync::CancellationToken;
 
-struct Worker<I, R, F>
-where
-    I: Send,
-    R: Send,
-    F: AsyncFn(I) -> R + Send + Sync + 'static,
-{
-    result_chan: mpsc::Sender<R>,
-    task_chan: mpsc::Receiver<I>,
-    handler: Arc<F>,
-    cancell: CancellationToken,
+#[async_trait::async_trait]
+pub trait AsyncJobHandler {
+    type Input: Send + 'static;
+    type Output: Send + 'static;
+
+    async fn handle(&self, input: Self::Input) -> Self::Output;
 }
 
+struct Worker<H: AsyncJobHandler> {
+    result_chan: mpsc::Sender<H::Output>,
+    task_chan: mpsc::Receiver<H::Input>,
+    handler: Arc<H>,
+    cancell: CancellationToken,
+}
 
 // right now it's an unordered subscription based model: you send some data -> you get some unordered output
-struct Pool<I, R, F>
-where
-    I: Send,
-    R: Send,
-    F: AsyncFn(I) -> R + Send + Sync + 'static,
-{
-    workers: Vec<Worker<I, R, F>>,
-    workers_channels: Vec<mpsc::Sender<I>>,
-    pub result_chan: mpsc::Receiver<R>,
+pub struct Pool<H: AsyncJobHandler> {
+    workers_channels: Vec<mpsc::Sender<H::Input>>,
+    pub result_chan: mpsc::Receiver<H::Output>,
     cancell: CancellationToken,
 }
 
-impl<I, R, F> Worker<I, R, F>
+impl<H: AsyncJobHandler> Worker<H>
 where
-    I: Send,
-    R: Send,
-    F: AsyncFn(I) -> R + Send + Sync + 'static,
+    H::Input: std::fmt::Debug,
 {
-    pub async fn run(mut self) {
+    pub async fn run(&mut self) {
         loop {
             tokio::select! {
                 _ = self.cancell.cancelled() => {
@@ -46,49 +37,52 @@ where
                 }
                 input = self.task_chan.recv() => {
                     if let Some(input) = input {
-                        let output = (self.handler)(input).await;
+                        println!("Received an input in worker: {:#?}", input);
+                        let output = self.handler.handle(input).await;
                         self.result_chan.send(output).await.unwrap();
                     }
                 }
             }
         }
     }
+    pub fn stop(&self) {
+        self.cancell.cancel()
+    }
 }
-impl<I, R, F> Pool<I, R, F>
+impl<H: AsyncJobHandler> Pool<H>
 where
-    I: Send,
-    R: Send,
-    F: AsyncFn(I) -> R + Send + Sync + 'static,
+    H::Input: std::fmt::Debug,
+    H: Sync + Send + 'static,
 {
-    pub fn new(worker_function: F, num_workers: usize, cancell: CancellationToken) -> Self {
+    pub fn new(handler: H, num_workers: usize, cancell: CancellationToken) -> Self {
         let (result_sender, result_receiver) = mpsc::channel(num_workers * 2);
-        let worker_arc = Arc::new(worker_function);
+        let worker_arc = Arc::new(handler);
 
-        let mut workers_channels: Vec<mpsc::Sender<I>> = vec![];
+        let mut workers_channels: Vec<mpsc::Sender<H::Input>> = vec![];
 
-        let workers: Vec<Worker<I, R, F>> = (0..num_workers)
-            .map(|_| {
-                let (task_sender, task_receiver) = mpsc::channel(num_workers * 2);
-                workers_channels.push(task_sender);
-                Worker {
-                    result_chan: result_sender.clone(),
-                    handler: worker_arc.clone(),
-                    cancell: cancell.child_token(),
-                    task_chan: task_receiver,
-                }
-            })
-            .collect();
+        for _ in 0..num_workers {
+            let (task_sender, task_receiver) = mpsc::channel(num_workers * 2);
+            workers_channels.push(task_sender);
 
+            let mut new_worker = Worker {
+                result_chan: result_sender.clone(),
+                handler: worker_arc.clone(),
+                cancell: cancell.child_token(),
+                task_chan: task_receiver,
+            };
+            tokio::spawn(async move {
+                new_worker.run();
+            });
+        }
         Self {
-            workers: workers,
-            workers_channels: workers_channels,
             result_chan: result_receiver,
-            cancell: cancell.clone(),
+            workers_channels: workers_channels,
+            cancell: cancell,
         }
     }
 
-    pub fn queue(&self, input: I) {
-        let index = fastrand::usize(..self.workers.len());
+    pub fn queue(&self, input: H::Input) {
+        let index = fastrand::usize(..self.workers_channels.len());
         let _ = self.workers_channels[index].send(input);
     }
 }
